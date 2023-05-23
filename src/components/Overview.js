54d0497ab9d7ba5532ad0802906b1b3ff4e0a126
Zen <amaar5ali@gmail.com>
2023-05-22 20:59:54 -0400

    final draft-add css, delete button, task number, edit button(optional)

---
+++ src/components/Overview.js
@@ -0,0 +1,17 @@
+// This file will display all our tasks
+
+import React from "react";
+
+const Overview = (props) => {
+    const { tasks } = props;
+
+    return (
+        <ul>
+            {tasks.map((task) => {
+                return <li key={task.id}>{task.text}</li>;
+            })}
+        </ul>
+    );
+}
+
+export default Overview;
